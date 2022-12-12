0738dc9100ff74b918c896541f6fa9b8b240cac6
NakamuraTsubasa88 <103015735+NakamuraTsubasa88@users.noreply.github.com>
2022-12-12 22:44:31 +0900

    Add files via upload

--- convert_ply-h5.py
+++ convert_ply-h5.py
@@ -2,26 +2,27 @@ import h5py
 import numpy as np
 from plyfile import PlyData, PlyElement
 import glob
+# import pprint as pp
 
 
 # 参考(github URL)
 # https://github.com/IsaacGuan/PointNet-Plane-Detection/blob/master/data/write_hdf5.py
+# https://isaacguan.github.io/2018/05/04/Prepare-Your-Own-Data-for-PointNet/
 
-#ファイル名指定
-filenames = glob.glob('ply/*.ply')
+# #ファイル名指定
+filenames = glob.glob('ply/wash_output/*.ply')
 
-#h5ファイルを作成する
-for i in range(len(filenames)):
-    f = h5py.File("h5/frame-output-%s.h5"%i, 'w')
-
-#plyファイルをh5形式に変換する
-a_data = np.zeros((len(filenames), 2048, 3))
-
-for i in range(0, len(filenames)):
+for i in range(0,len(filenames)):
+    #データの読込み
     plydata = PlyData.read(filenames[i])
-    for j in range(0, 2048):
-        a_data[i, j] = [plydata['vertex']['x'][j], plydata['vertex']['y'][j], plydata['vertex']['z'][j]]
-
-#h5ファイルに書き込み
-data = f.create_dataset("data", data = a_data)
+    #点群の個数で配列を作成([x軸の値,x軸の値,x軸の値]*点群の個数分)
+    a_data = np.empty((1, plydata.elements[0].count, 3))
+    #ply→h5変換
+    for j in range(0, plydata.elements[0].count):
+        a_data[0, j] = [plydata['vertex']['x'][j], plydata['vertex']['y'][j], plydata['vertex']['z'][j]]
+    #h5ファイルを作成する
+    f = h5py.File("h5/wash_h5/wash-frame-output-%s.h5"%i, 'w')
+    #h5にデータを書き込む
+    data = f.create_dataset("data", data = a_data)
+    # pp.pprint(data)
 
